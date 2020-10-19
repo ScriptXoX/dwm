e0ec6ec9e3d85f267cc822aed7b2d8b00a49b187
ScriptXoX <scriptxox@163.com>
2020-10-19 09:30:36 +0800

    smartgaps 修正

--- config.def.h
+++ config.def.h
@@ -9,7 +9,7 @@ static const unsigned int gappih    = 6;       /* horiz inner gap between window
 static const unsigned int gappiv    = 6;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 6;       /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov    = 6;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 4;        /* 1 means no outer gap when there is only one window */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 
 
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
