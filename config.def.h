ccc909d810c838f32311d24e17a637f654745278
ScriptXoX <scriptxox@163.com>
2020-10-20 07:17:42 +0800

    set autostart

--- config.def.h
+++ config.def.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 
 
@@ -21,9 +21,10 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const Bool viewontag         = False;     /* Switch view on tag switch */
-static const char *fonts[]          = { "Liberation Mono:size=10" };
-static const char dmenufont[]       = "Liberation Mono:size=10";
+static const Bool viewontag         = True;     /* Switch view on tag switch */
+//static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "monospace:size=10", "JoyPixels:pixelsize=10:antialias=true:autohint=true"  };
+static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -56,7 +57,9 @@ static Sp scratchpads[] = {
 
 static const char *const autostart[] = {
 	"sh", "-c","~/.dwm/autostart.sh",NULL, /* terminate */
-	"sh", "-c","while true;do ~/.dwm/bar.sh;sleep 1;done;",NULL /* bar */
+	"sh", "-c","~/.dwm/bar.sh",NULL, /* terminate */
+	"sh", "-c","~/.dwm/clock.sh",NULL, /* terminate */
+	//"sh", "-c","while true;do ~/.dwm/bar.sh;sleep 1;done;",NULL /* bar */
 
 };
 
