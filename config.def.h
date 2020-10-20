5e2d6616c20d931e73a07a155bf92b3ba1e70144
ScriptXoX <scriptxox@163.com>
2020-10-20 10:02:45 +0800

    dwm-rotatestack-20161021-ab9571b.diff

--- config.def.h
+++ config.def.h
@@ -120,6 +120,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
 
+	{ MODKEY|ShiftMask,             XK_j,      rotatestack,    {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_k,      rotatestack,    {.i = -1 } },
+
   /*{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },*/
 	
