c056e552bbf284395a0bdea91a619bf18b65d86c
ScriptXoX <scriptxox@163.com>
2022-09-14 20:27:59 +0800

    patch dwm-systray-6.3.diff

--- config.def.h
+++ config.def.h
@@ -3,10 +3,23 @@
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int gappx     = 10;        /* gaps between windows */
-static const unsigned int snap      = 32;       /* snap pixel */
 
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+
+
+static const unsigned int snap      = 32;       /* snap pixel */
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 10;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;     /* 0 means no systray */
+
+
+static const int showbar            = 1;     /* 0 means no bar */
+static const int topbar             = 1;     /* 0 means bottom bar */
+
+
+
+
 #define ICONSIZE 28   /* icon size */
 #define ICONSPACING 6 /* space between icon and title */
 
@@ -16,8 +29,8 @@ static const int horizpadbar        = 10;        /* horizontal padding for statu
 static const int vertpadbar         = 10;       /* vertical padding for statusbar */
 
 //与屏幕间距 
-static const int vertpad            = 10;       /* vertical padding of bar */  
-static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int vertpad            = 0;       /* vertical padding of bar */  
+static const int sidepad            = 0;       /* horizontal padding of bar */
 
 //static const char *fonts[]          = { "monospace:size=10", "NotoColorEmoji:pixelsize=24:antialias=true"  };
 static const char *fonts[]          ={"Monaco:pixelsize=24:antialias=true", "NotoColorEmoji:pixelsize=24:antialias=true"};
@@ -55,17 +68,17 @@ static Sp scratchpads[] = {
 
 
 static const char *const autostart[] = {
-	"dwmblocks",NULL,
-	"redshift",NULL,
-	"fcitx5",NULL,
-	"picom","-b","--experimental-backends",NULL,
-    "sh","-c","sudo echo 3 | sudo tee /proc/sys/net/ipv4/tcp_fastopen ",NULL,
-    "trojan-go","-config","/etc/trojan/config.json",NULL,
-    "sh","-c","feh --bg-center ~/wallpaper/01.jpg",NULL,
-    "xmodmap ~/.xmodmaprc",NULL,
-    "wmname","LG3D",NULL,
-    "batsignal","-b",NULL,
-    "twmnd",NULL,
+////	"dwmblocks",NULL,
+//	"redshift",NULL,
+//	"fcitx5",NULL,
+//	"picom","-b","--experimental-backends",NULL,
+//    "sh","-c","sudo echo 3 | sudo tee /proc/sys/net/ipv4/tcp_fastopen ",NULL,
+//    "trojan-go","-config","/etc/trojan/config.json",NULL,
+//    "sh","-c","feh --bg-center ~/wallpaper/01.jpg",NULL,
+//    "xmodmap ~/.xmodmaprc",NULL,
+//    "wmname","LG3D",NULL,
+//    "batsignal","-b",NULL,
+//    "twmnd",NULL,
    "sh", "-c","~/.dwm/clock.sh",NULL, 
    //"sh", "-c","~/.dwm/battery-status.sh",NULL,
  //   "sh","-c","~/.dwm/bin/terminal",NULL
@@ -199,8 +212,8 @@ static const Key keys[] = {
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
 static const Button buttons[] = {
 	/* click                event mask      button          function        argument */
-	{ ClkLtSymbol,          0,              Button1,        setlayout,      {0} },
-	{ ClkLtSymbol,          0,              Button3,        setlayout,      {.v = &layouts[2]} },
+	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
+	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 	{ ClkWinTitle,          0,              Button2,        zoom,           {0} },
 	{ ClkStatusText,        0,              Button2,        spawn,          {.v = termcmd } },
 	{ ClkClientWin,         MODKEY,         Button1,        movemouse,      {0} },
